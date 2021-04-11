960b6216ed6443c652a7934a01ed10f65fd01af4
tetsuya-ito-kronos <tetsuya_ito@kronos.jp>
2021-04-11 15:38:05 +0900

    first commit

---
+++ js/data.js
@@ -0,0 +1,214 @@
+const userData = {
+  1: '早津大',
+  2: '長野智哉',
+  3: '錦織拓人',
+  4: '江村栞',
+  5: '山本健太郎',
+  6: '安福香花',
+  7: '毎熊研志郎',
+  8: '佐野遥',
+  9: '近藤輝',
+};
+
+const teamData = {
+  1: 'ライオンさん組',
+  2: 'ねこさん組',
+  3: 'ぞうさん組',
+};
+
+// key:チームID、value:userId配列
+const relationTeamData = {
+  1: [1, 2, 3],
+  2: [4, 5, 6],
+  3: [7, 8, 9],
+}
+
+const gameData = {
+  1: 'イントロクイズ',
+  2: 'ワードウルフ',
+  3: '箱の中身はなんだろな',
+  4: '芸能人名前当てクイズ',
+};
+
+const scoreData = [
+  {
+    userId: 1,
+    gameId: 1,
+    score: 30,
+  },
+  {
+    userId: 2,
+    gameId: 1,
+    score: 33,
+  },
+  {
+    userId: 3,
+    gameId: 1,
+    score: 21,
+  },
+  {
+    userId: 4,
+    gameId: 1,
+    score: 10,
+  },
+  {
+    userId: 5,
+    gameId: 1,
+    score: 9,
+  },
+  {
+    userId: 6,
+    gameId: 1,
+    score: 4,
+  },
+  {
+    userId: 7,
+    gameId: 1,
+    score: 38,
+  },
+  {
+    userId: 8,
+    gameId: 1,
+    score: 11,
+  },
+  {
+    userId: 9,
+    gameId: 1,
+    score: 22,
+  },
+  {
+    userId: 1,
+    gameId: 2,
+    score: 23,
+  },
+  {
+    userId: 2,
+    gameId: 2,
+    score: 55,
+  },
+  {
+    userId: 3,
+    gameId: 2,
+    score: 61,
+  },
+  {
+    userId: 4,
+    gameId: 2,
+    score: 53,
+  },
+  {
+    userId: 5,
+    gameId: 2,
+    score: 72,
+  },
+  {
+    userId: 6,
+    gameId: 2,
+    score: 34,
+  },
+  {
+    userId: 7,
+    gameId: 2,
+    score: 31,
+  },
+  {
+    userId: 8,
+    gameId: 2,
+    score: 45,
+  },
+  {
+    userId: 9,
+    gameId: 2,
+    score: 62,
+  },
+  {
+    userId: 1,
+    gameId: 3,
+    score: 58,
+  },
+  {
+    userId: 2,
+    gameId: 3,
+    score: 24,
+  },
+  {
+    userId: 3,
+    gameId: 3,
+    score: 45,
+  },
+  {
+    userId: 4,
+    gameId: 3,
+    score: 56,
+  },
+  {
+    userId: 5,
+    gameId: 3,
+    score: 32,
+  },
+  {
+    userId: 6,
+    gameId: 3,
+    score: 60,
+  },
+  {
+    userId: 7,
+    gameId: 3,
+    score: 33,
+  },
+  {
+    userId: 8,
+    gameId: 3,
+    score: 45,
+  },
+  {
+    userId: 9,
+    gameId: 3,
+    score: 23,
+  },
+  {
+    userId: 1,
+    gameId: 4,
+    score: 21,
+  },
+  {
+    userId: 2,
+    gameId: 4,
+    score: 33,
+  },
+  {
+    userId: 3,
+    gameId: 4,
+    score: 59,
+  },
+  {
+    userId: 4,
+    gameId: 4,
+    score: 34,
+  },
+  {
+    userId: 5,
+    gameId: 4,
+    score: 13,
+  },
+  {
+    userId: 6,
+    gameId: 4,
+    score: 49,
+  },
+  {
+    userId: 7,
+    gameId: 4,
+    score: 42,
+  },
+  {
+    userId: 8,
+    gameId: 4,
+    score: 33,
+  },
+  {
+    userId: 9,
+    gameId: 4,
+    score: 42,
+  },
+];
---
+++ js/table.js
@@ -0,0 +1,147 @@
+// dataはこのグローバル変数を参照してください。
+let users;
+let teams;
+let relationTeam;
+let games;
+let scores;
+
+let table;
+const TEAM = '1';
+const GAME = '2';
+const USER = '3';
+
+window.onload = () => {
+  // data.js内データを参照_ここは変えないでください。
+  users = userData;
+  teams = teamData;
+  relationTeam = relationTeamData;
+  games = gameData;
+  scores = scoreData;
+  // tableDom取得
+  table = document.getElementById('table');
+  // イベント設定
+  const select = document.getElementById('kind');
+  select.onchange = e => createTable(e.target.value);
+  // 初期表示
+  createTable(select.value);
+};
+
+createTable = (kind) => {
+  table.innerHTML = null;
+  try {
+    switch (kind) {
+      case TEAM:
+        return createTeamTable();
+      case GAME:
+        return createGameTable();
+      case USER:
+        return createUserTable();
+      default:
+        throw '存在しない種別';
+    }
+  } catch (err) {
+    console.log(err);
+    const error = document.createElement('p');
+    error.innerText = 'エラーが発生しました！';
+    document.body.appendChild(error);
+  }
+};
+
+createTeamTable = () => {
+  const rows = Array.from(
+    scores
+      .reduce((map, target) => {
+        const key = getTeamId(target.userId);
+        if (map.has(key)) {
+          map.get(key).push(target);
+        } else {
+          map.set(key, [target]);
+        }
+        return map;
+      }, new Map())
+    )
+    .map(([key, userArray]) =>
+      userArray.reduce((user, target) => {
+        user.score += target.score;
+        return user;
+      }, { id: key, score: 0 })
+    )
+    .sort((prev, target) => target.score - prev.score);
+  
+  rows.forEach(row => {
+    const tr = document.createElement('tr');
+    const teamTd = document.createElement('td');
+    teamTd.innerText = teams[row.id];
+    const scoreTd = document.createElement('td');
+    scoreTd.innerText = row.score;
+    tr.appendChild(teamTd);
+    tr.appendChild(scoreTd);
+    table.appendChild(tr);
+  });
+};
+
+createGameTable = () => {
+  const rows = Object.keys(games).map(gameKey =>
+    scores
+      .filter(score => score.gameId === Number(gameKey))
+      .reduce((prev, target) => prev.score > target.score ? prev : target)
+  );
+
+  rows.forEach(row => {
+    const tr = document.createElement('tr');
+    const gameTd = document.createElement('td');
+    gameTd.innerText = games[row.gameId];
+    const nameTd = document.createElement('td');
+    nameTd.innerText = users[row.userId];
+    const teamTd = document.createElement('td');
+    teamTd.innerText = teams[getTeamId(row.userId)];
+    const scoreTd = document.createElement('td');
+    scoreTd.innerText = row.score;
+    tr.appendChild(gameTd);
+    tr.appendChild(nameTd);
+    tr.appendChild(teamTd);
+    tr.appendChild(scoreTd);
+    table.appendChild(tr);
+  });
+};
+
+createUserTable = () => {
+  const rows = Array.from(
+    scores
+      .reduce((map, target) => {
+        const key = target.userId;
+        if (map.has(key)) {
+          map.get(key).push(target);
+        } else {
+          map.set(key, [target]);
+        }
+        return map;
+      }, new Map())
+    )
+    .map(([key, userArray]) =>
+      userArray.reduce((user, target) => {
+        user.score += target.score;
+        return user;
+      }, { id: key, score: 0 })
+    )
+    .sort((prev, target) => target.score - prev.score);
+  
+  rows.forEach(row => {
+    const tr = document.createElement('tr');
+    const nameTd = document.createElement('td');
+    nameTd.innerText = users[row.id];
+    const teamTd = document.createElement('td');
+    teamTd.innerText = teams[getTeamId(row.id)];
+    const scoreTd = document.createElement('td');
+    scoreTd.innerText = row.score;
+    tr.appendChild(nameTd);
+    tr.appendChild(teamTd);
+    tr.appendChild(scoreTd);
+    table.appendChild(tr);
+  });
+};
+
+getTeamId = (userId) => {
+  return Object.entries(relationTeam)
+    .find(([key, value]) => value.includes(userId))[0];
+};
